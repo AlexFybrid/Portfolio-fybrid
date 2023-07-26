fcedc5d6aa70d4effc0f42a1d20263237faba2cf
AlexFybrid <124744445+AlexFybrid@users.noreply.github.com>
2023-07-26 15:10:57 +0300

    fix

--- files/script /script.js
+++ files/script /script.js
@@ -29,7 +29,6 @@ const lightbox = new PhotoSwipeLightbox({
   zoomSVG: nothing,
   padding: { top: 50, bottom: 50, left: 50, right: 50 },
   mouseMovePan: false,
-  wheelToZoom: true,
   secondaryZoomLevel: 0.6,
   imageClickAction: 'close',
   showAnimationDuration: 250,
